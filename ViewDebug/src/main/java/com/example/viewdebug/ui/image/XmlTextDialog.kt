b1ae4a74ef94ef1f8fbcf41801ac60fd878a5064
qinf <qinf@meixing.com>
2023-06-29 15:18:41 +0800

    CORE

--- ViewDebug/src/main/java/com/example/viewdebug/ui/image/XmlTextDialog.kt
+++ ViewDebug/src/main/java/com/example/viewdebug/ui/image/XmlTextDialog.kt
@@ -1,5 +1,7 @@
 package com.example.viewdebug.ui.image
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
@@ -8,7 +10,7 @@ import com.example.viewdebug.databinding.ViewDebugXmlTextContainerBinding
 import com.example.viewdebug.ui.UIPage
 
 class XmlTextDialog(
-    ctx: Context,
+    private val ctx: Context,
     private val hostPage: UIPage,
 ) {
     private val binding = ViewDebugXmlTextContainerBinding.inflate(LayoutInflater.from(ctx), hostPage.tabView.parent as ViewGroup, false)
@@ -19,6 +21,14 @@ class XmlTextDialog(
     fun show(title: String, xml: CharSequence) {
         binding.tvName.text = title
         binding.tvText.text = xml
+        binding.tvName.setOnClickListener {
+            copyToClipboard(ctx, title.split("/")[1])
+        }
         hostPage.showDialog(binding.root)
+    }
+
+    private fun copyToClipboard(ctx: Context, text: String) {
+        val clipboardManager = ctx.getSystemService(ClipboardManager::class.java)
+        clipboardManager.setPrimaryClip(ClipData.newPlainText("UI调试", text))
     }
 }
