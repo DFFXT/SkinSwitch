673f02f10b1a00d9bc71aa2c0b79ef826c23d102
非想非 <1559379562@qq.com>
2023-07-18 21:54:01 +0800

    支持新增id，单个文件最多15个

---
+++ ViewDebug/src/main/java/com/example/viewdebug/xml/struct/ReferenceType.kt
@@ -0,0 +1,11 @@
+package com.example.viewdebug.xml.struct
+
+annotation class ReferenceType {
+    companion object {
+        const val TYPE_ID = "id"
+        const val TYPE_DRAWABLE = "drawable"
+        const val TYPE_COLOR = "color"
+        const val TYPE_DIMEN = "dimen"
+        const val TYPE_STRING = "string"
+    }
+}
--- ViewDebug/src/main/java/com/example/viewdebug/xml/struct/writer/helper/value/AttReferenceValueCompile.kt
+++ ViewDebug/src/main/java/com/example/viewdebug/xml/struct/writer/helper/value/AttReferenceValueCompile.kt
@@ -1,6 +1,8 @@
 package com.example.viewdebug.xml.struct.writer.helper.value
 
+import com.example.viewdebug.R
 import com.example.viewdebug.ViewDebugInitializer
+import com.example.viewdebug.xml.struct.ReferenceType
 import com.example.viewdebug.xml.struct.XmlCompiler
 import com.example.viewdebug.xml.struct.writer.helper.ResourceType
 
@@ -11,6 +13,32 @@ import com.example.viewdebug.xml.struct.writer.helper.ResourceType
  * @color/xxx
  */
 class AttReferenceValueCompile : AttrValueCompile("reference") {
+    /**
+     * id模板，如果更新的布局中新增了id，ConstraintLayout布局中很常见，那么就用这些替换，最多支持15个新增id
+     */
+    private val idTemplate by lazy {
+        intArrayOf(
+            R.id.view_debug_template_id_1,
+            R.id.view_debug_template_id_2,
+            R.id.view_debug_template_id_3,
+            R.id.view_debug_template_id_4,
+            R.id.view_debug_template_id_5,
+            R.id.view_debug_template_id_6,
+            R.id.view_debug_template_id_7,
+            R.id.view_debug_template_id_8,
+            R.id.view_debug_template_id_9,
+            R.id.view_debug_template_id_10,
+            R.id.view_debug_template_id_11,
+            R.id.view_debug_template_id_12,
+            R.id.view_debug_template_id_13,
+            R.id.view_debug_template_id_14,
+            R.id.view_debug_template_id_15,
+        )
+    }
+    // 当前使用的id下标
+    private var idIndex = 0
+    // key: id 名称，value：id值
+    private val idMap = HashMap<String, Int>()
     override fun compile(attrValue: String, compiler: XmlCompiler): CompiledAttrValue? {
         // 是引用类型
         val end = attrValue.indexOf('/')
@@ -21,9 +49,25 @@ class AttReferenceValueCompile : AttrValueCompile("reference") {
             val ctx = ViewDebugInitializer.ctx
             if (valueRowType == "+id") {
                 // 单独判断@+id的情况
-                valueRowType = "id"
+                valueRowType = ReferenceType.TYPE_ID
             }
-            val id = ctx.resources.getIdentifier(valueRowValue, valueRowType, ctx.packageName)
+            var id = ctx.resources.getIdentifier(valueRowValue, valueRowType, ctx.packageName)
+            if (id == 0 && valueRowType == ReferenceType.TYPE_ID) {
+                // 是新增的id
+                var reflexId = idMap[valueRowValue]
+                // 需要使用新的id
+                if (reflexId == null) {
+                    if (idIndex >= idTemplate.size) {
+                        throw Exception("increase new id over 15")
+                    } else {
+                        reflexId = idTemplate[idIndex]
+                        idMap[valueRowValue] = reflexId
+                        idIndex++
+                    }
+                }
+                id = reflexId
+
+            }
             // todo
             return CompiledAttrValue(ResourceType.TYPE_REFERENCE, id)
         }
