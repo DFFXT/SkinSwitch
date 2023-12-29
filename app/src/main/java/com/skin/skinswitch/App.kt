e6230f5dcfaaad14fcf941c262e10a846f1fcc03
qinf <qinf@meixing.com>
2023-12-29 15:54:13 +0800

    [变更内容]：如果不加载外部皮肤包，而且模式跟随系统可以直接使用context的默认resource [ID]:无 [影响范围]：无 [限制事项]：无

--- app/src/main/java/com/skin/skinswitch/App.kt
+++ app/src/main/java/com/skin/skinswitch/App.kt
@@ -1,13 +1,20 @@
 package com.skin.skinswitch
 
 import android.app.Application
+import android.content.Context
 import android.content.res.Resources
 import com.CustomViewTestSkinView
 import com.example.skinswitch.R
 import com.skin.skincore.SkinManager
 import com.skin.skincore.apply.base.BaseViewApply
+import com.skin.skincore.asset.Asset
+import com.skin.skincore.asset.AssetLoaderManager
+import com.skin.skincore.asset.IAsset
+import com.skin.skincore.asset.IAssetFactory
 import com.skin.skincore.provider.DefaultProviderFactory
 import com.skin.skincore.provider.IResourceProvider
+import com.skin.skincore.provider.ISkinPathProvider
+import com.skin.skincore.provider.MergeResource
 import com.skin.skinswitch.const.AppConst
 
 class App : Application() {
@@ -23,6 +30,20 @@ class App : Application() {
     }
 
     private fun initSkin() {
+        AssetLoaderManager.setAssetFactory(object : IAssetFactory {
+            override fun createAsset(context: Context, skinPathProvider: ISkinPathProvider): IAsset {
+                return object : Asset(context, skinPathProvider) {
+                    override fun getResource(): Resources {
+                        val res = context.resources
+                        if (res is MergeResource) {
+                            return res.default
+                        }
+                        return super.getResource()
+                    }
+                }
+            }
+
+        })
         SkinManager.init(
             this, R.style.Theme_SkinSwitch,
             object : DefaultProviderFactory() {
