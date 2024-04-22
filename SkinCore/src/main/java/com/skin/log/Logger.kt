56b2d82c72bdcf387fa24872caa728e1e8300cf4
非想非 <1559379562@qq.com>
2024-04-22 22:39:28 +0800

    支持取消换肤框架的log

--- SkinCore/src/main/java/com/skin/log/Logger.kt
+++ SkinCore/src/main/java/com/skin/log/Logger.kt
@@ -4,28 +4,28 @@ package com.skin.log
  * 换肤框架里面的日志工具
  */
 object Logger : ILogger {
-    private var logger: ILogger = DefaultLogger()
+    private var logger: ILogger? = DefaultLogger()
 
     /**
      * 设置log外部实现
      */
-    fun setLoggerImpl(logger: ILogger) {
+    fun setLoggerImpl(logger: ILogger?) {
         this.logger = logger
     }
 
     override fun v(tag: String, msg: String?) {
-        logger.v(tag, msg)
+        logger?.v(tag, msg)
     }
 
     override fun d(tag: String, msg: String?) {
-        logger.d(tag, msg)
+        logger?.d(tag, msg)
     }
 
     override fun i(tag: String, msg: String?) {
-        logger.i(tag, msg)
+        logger?.i(tag, msg)
     }
 
     override fun e(tag: String, msg: String?) {
-        logger.e(tag, msg)
+        logger?.e(tag, msg)
     }
 }
